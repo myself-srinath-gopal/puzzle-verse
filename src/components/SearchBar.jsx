d438be43ab26143aa596728147276900575e0bd2
chris-fletcher-1 <fletchergitmail@gmail.com>
2025-11-15 12:11:31 +0530

    final commit

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -23,9 +23,8 @@ const SearchBar = () => {
 
     return showSearch && visible ? (
         <div className='border-t border-gray-200 bg-gray-50 text-center'>
-            <div className="inline-flex items-center justify-center border border-gray-400 px-5 py-2 my-5 mx-3 rounded-full w-3/4 sm:w-1/2">
+            <div className="inline-flex items-center justify-center border border-gray-400 px-2 sm:px-5 py-2 my-5 mx-3 rounded-full w-3/4 sm:w-1/2">
                 <input value={filters.searchTerm} onChange={e => dispatch(setFilters({ searchTerm: e.target.value }))} type="text" placeholder='Search' className="flex-1 outline-none bg-inherit text-sm" />
-                <img className='w-4' src={assets.search_icon} alt="" />
             </div>
             <button onClick={() => setShowSearch(false)} className='inline cursor-pointer'>
                 <img className='w-3' src={assets.cross_icon} alt="" />
--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -25,9 +25,6 @@ const Product = () => {
         <div className='border-t border-gray-300 pt-10 transition-opacity ease-in duration-500 opacity-100'>
             <div className="flex gap-2 sm:gap-12 flex-col sm:flex-row">
                 <div className="flex-1 flex flex-col-reverse gap-3 sm:flex-row">
-                    <div className="flex sm:flex-col overflow-x-auto sm:overflow-y-scroll justify-between sm:justify-normal sm:w-[18.7%] w-full">
-                        <img src={product.image[0]} alt="" className='w-24% sm:w-full sm:mb-3 shrink-0 cursor-pointer' />
-                    </div>
                     <div className="w-full sm:w-[80%]">
                         <img src={product.image[0]} alt="" className='w-full h-auto' />
                     </div>
